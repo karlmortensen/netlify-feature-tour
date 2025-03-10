afd124a35063c4d92b0fcf1a95cc6258077b450a
Karl Mortensen <karlmortensen@gmail.com>
2025-03-10 17:30:56 -0600

    try a function III

--- netlify/functions/hello-world.js
+++ netlify/functions/hello-world.js
@@ -1,8 +1,9 @@
 exports.handler = async () => {
 const tc = process.env.TACO_COUNT;
 const ov = process.env.CAT_COUNT;
+const bc = process.env.BEANS_COUNT;
     return {
     statusCode: 200,
-    body: "Word dog. tc is ${tc} and ${ov}"
+    body: 'Word dog. tc is ${tc} and ${ov} ${bc}'
     };
 };
